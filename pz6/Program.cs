485516c6d943e8562921d8586ed418cba1f3997f
asfasdfgasdfasdf@outlook.com <asfasdfgasdfasdf@outlook.com>
2022-09-29 09:40:12 +0300

    практика 6

---
+++ pz6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace pz6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите число для возведения в квадраты и куби: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = a;
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(b);
+                i++;
+                b *= 2;
+            }
+            i = 0;
+            b = a;
+            Console.WriteLine("");
+            while (i < 10)
+            {
+                Console.WriteLine(b);
+                i++;
+                b = b * a * a;
+            }
+        }
+    }
+}
