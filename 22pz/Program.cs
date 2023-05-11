23df19414d9120d4099624f047470423b4bc1123
Илья Тулинцев <den539321@gmail.com>
2023-05-12 00:09:57 +0500

    22

---
+++ 22pz/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Net.Sockets;
+
+namespace _22pz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime otprav = new DateTime(2023, 2, 20, 12, 30, 00);
+            Ticket sasha = new Ticket(2654, 564905, "льгот", 7, 11, otprav);
+            sasha.PrintInfo();
+            sasha.PrintInfo();
+
+
+        }
+    }
+}
