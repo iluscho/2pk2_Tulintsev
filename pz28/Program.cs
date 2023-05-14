81268f540d2b5d6fde3fbdddba5aafe4f5f06a24
Илья Тулинцев <den539321@gmail.com>
2023-05-15 00:41:38 +0500

    pz28

---
+++ pz28/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+class TemperatureSensor
+{
+    public event EventHandler<int> TemperatureChanged;
+
+    public void CheckTemperature(int temperature)
+    {
+        if (temperature == -25 || temperature == 0 || temperature == 25 || temperature == 35)
+            OnTemperatureChanged(temperature);
+    }
+
+    protected virtual void OnTemperatureChanged(int temperature)
+    {
+        TemperatureChanged?.Invoke(this, temperature);
+    }
+}
+
+class TemperatureController
+{
+    private int _temperature = -30;
+    private readonly TemperatureSensor _sensor = new TemperatureSensor();
+
+    public TemperatureController()
+    {
+        _sensor.TemperatureChanged += (sender, temperature) =>
+        {
+            Console.WriteLine($"Температура достига отметки в {temperature} градусов!");
+        };
+    }
+
+    public void Start()
+    {
+        while (_temperature <= 40)
+        {
+            _temperature++;
+            Console.WriteLine($"Температура в реальном времени: {_temperature} градусов");
+            _sensor.CheckTemperature(_temperature);
+            Thread.Sleep(1000); // Задержка в 1 секунду
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var controller = new TemperatureController();
+        controller.Start();
+    }
+}
---
+++ pz28_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем автомобиль и патрульного
+            var car = new Car();
+            var patrol = new Patrol();
+
+            // Подписываемся на события автомобиля
+            car.SpeedChanged += (sender, speed) =>
+            {
+                if (speed > 80 && speed <= 120)
+                {
+                    patrol.Warn();
+                }
+                else if (speed > 120)
+                {
+                    patrol.Arrest();
+                }
+            };
+
+            // Запускаем автомобиль
+            car.Start();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        private int _speed;
+        private bool _started;
+
+        public event EventHandler<int> SpeedChanged;
+
+        public void Start()
+        {
+            if (!_started)
+            {
+                _started = true;
+                Accelerate();
+            }
+        }
+
+        private async void Accelerate()
+        {
+            for (int i = 0; i <= 200; i++)
+            {
+                _speed = i;
+                SpeedChanged?.Invoke(this, i);
+                await System.Threading.Tasks.Task.Delay(100);
+                Console.WriteLine($"Скорость автомобиля: {_speed}");
+            }
+        }
+    }
+
+    class Patrol
+    {
+        public void Warn()
+        {
+            Console.WriteLine("Превышена скорость! Снизьте скорость до 80 км/ч");
+        }
+
+        public void Arrest()
+        {
+            Console.WriteLine("Превышена скорость! Активизирован режим задержания!");
+        }
+    }
+}
