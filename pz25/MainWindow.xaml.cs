92e5a2463a9fe95789080b905e321bc93b1adeea
Илья Тулинцев <den539321@gmail.com>
2023-05-15 01:32:00 +0500

    pz25

---
+++ pz25/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace pz25
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(FirstNumberTextBox.Text, out double firstNumber) ||
+                !double.TryParse(SecondNumberTextBox.Text, out double secondNumber))
+            {
+                ((Button)sender).Content = "er37or";
+                return;
+            }
+
+            double result = firstNumber + secondNumber;
+            ((Button)sender).Content = result;
+        }
+    }
+}
